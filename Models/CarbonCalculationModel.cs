76ff3818109d09b7339c5360a95847740c8915d3
Peter L <eternalcoffee@hotmail.com>
2017-08-26 14:59:20 +1200

    Controller control model

---
+++ Models/CarbonCalculationModel.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ecoMuffins.Models
+{
+    public class CarbonCalculationModel{
+        public JourneyType TransportType {get; set;}
+        public double Distance { get; set; }
+    }
+    public enum JourneyType 
+    {
+        Bus = 0,
+        Train = 1,
+        Bike = 2,
+        Walk = 3,
+        Car = 4
+    };
+}
